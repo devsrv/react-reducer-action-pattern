834b77b0e5f7b709ee65e0f62508e85e1262b051
Sourav R <developer.srv1@gmail.com>
2020-07-12 21:23:03 +0530

    simple workflow of fetch posts & remove post with useReducer

---
+++ src/reducer/Post.js
@@ -0,0 +1,24 @@
+export const initialState = {
+    allPosts: [],
+};
+
+export function PostsReducer(prevState, action) {
+    switch (action.type) {
+        case 'SET_POSTS':
+            return {
+                ...prevState,
+                allPosts: action.payload.posts,
+            };
+        case 'REMOVE_POST':
+            const postIndex = prevState.allPosts.findIndex(p => p.id === Number(action.payload.id));
+            
+            return {
+                ...prevState,
+                allPosts: [...prevState.allPosts.slice(0, postIndex), ...prevState.allPosts.slice(postIndex + 1)]
+            };
+        default:
+            return {
+                ...prevState,
+            };
+    }
+}
